bc6a7b572daf2bfdf483f40a943b41b387b2a2dc
unknown <ercheung3@697-334-Dale.sf.k12.internal>
2014-09-10 11:13:36 -0700

    Lightning with fade

--- Lightning.pde
+++ Lightning.pde
@@ -6,23 +6,26 @@ int endY=150;
 
 void setup()
 {
-	size(300,300);
+	size(600,300);
 	background(0,0,0);
-	strokeWeight(0.5);
+	strokeWeight(0.25);
+	frameRate(200);
 
 }
 void draw()
 {
-	stroke(0,startX,startX);
-	while(endX<300)
+	stroke(0,startX,startY);
+	if(startX<600)
 	{
 		endX=startX+(int)(Math.random()*10);
-		endY=startY+(int)(Math.random()*20)-9;
+		endY=startY+(int)(Math.random()*15)-7;
 		line(startX,startY,endX,endY);
 		startX=endX;
 		startY=endY;
 		stroke(0,startX,startY);
 	}
+	fill(0,0,0,7);
+	rect(0,0,600,300);
 
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -22,23 +22,26 @@ int endY=150;
 
 public void setup()
 {
-	size(300,300);
+	size(600,300);
 	background(0,0,0);
-	strokeWeight(0.5f);
+	strokeWeight(0.25f);
+	frameRate(200);
 
 }
 public void draw()
 {
-	stroke(0,startX,startX);
-	while(endX<300)
+	stroke(0,startX,startY);
+	if(startX<600)
 	{
 		endX=startX+(int)(Math.random()*10);
-		endY=startY+(int)(Math.random()*20)-9;
+		endY=startY+(int)(Math.random()*15)-7;
 		line(startX,startY,endX,endY);
 		startX=endX;
 		startY=endY;
 		stroke(0,startX,startY);
 	}
+	fill(0,0,0,7);
+	rect(0,0,600,300);
 
 }
 
